f7be85951cdaf3fce3be9b8b55d91f7a30e84f74
walan-create <116348714+walan-create@users.noreply.github.com>
2025-09-26 00:39:18 +0200

    cards inclinadas

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -6,4 +6,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './projects.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsComponent { }
+export class ProjectsComponent { 
+tiltCard(event: MouseEvent) {
+  const card = event.currentTarget as HTMLElement;
+  const rect = card.getBoundingClientRect();
+  const x = event.clientX - rect.left; // posición del ratón dentro de la tarjeta
+  const y = event.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = ((y - centerY) / centerY) * -15; // negativo para efecto invertido
+  const rotateY = ((x - centerX) / centerX) * 15;
+
+  card.style.setProperty('--rotate-x', `${rotateX}deg`);
+  card.style.setProperty('--rotate-y', `${rotateY}deg`);
+}
+
+resetTilt(event: MouseEvent) {
+  const card = event.currentTarget as HTMLElement;
+  card.style.setProperty('--rotate-x', `0deg`);
+  card.style.setProperty('--rotate-y', `0deg`);
+}
+
+}
