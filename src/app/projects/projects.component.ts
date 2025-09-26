b6bd94e75f377f09df6352cb566764052aa13b80
walan-create <116348714+walan-create@users.noreply.github.com>
2025-09-26 18:35:22 +0200

    Semiterminado

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  signal,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,27 +13,31 @@ import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angu
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent {
+  showAll = signal<Boolean>(false);
 
-tiltCard(event: MouseEvent) {
-  const card = event.currentTarget as HTMLElement;
-  const rect = card.getBoundingClientRect();
-  const x = event.clientX - rect.left; // posición del ratón dentro de la tarjeta
-  const y = event.clientY - rect.top;
+  toggleShowAll(): void {
+    this.showAll.set(!this.showAll());
+  }
 
-  const centerX = rect.width / 2;
-  const centerY = rect.height / 2;
+  tiltCard(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left; // posición del ratón dentro de la tarjeta
+    const y = event.clientY - rect.top;
 
-  const rotateX = ((y - centerY) / centerY) * -5; // negativo para efecto invertido
-  const rotateY = ((x - centerX) / centerX) * 5;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
 
-  card.style.setProperty('--rotate-x', `${rotateX}deg`);
-  card.style.setProperty('--rotate-y', `${rotateY}deg`);
-}
+    const rotateX = ((y - centerY) / centerY) * -5; // negativo para efecto invertido
+    const rotateY = ((x - centerX) / centerX) * 5;
 
-resetTilt(event: MouseEvent) {
-  const card = event.currentTarget as HTMLElement;
-  card.style.setProperty('--rotate-x', `0deg`);
-  card.style.setProperty('--rotate-y', `0deg`);
-}
+    card.style.setProperty('--rotate-x', `${rotateX}deg`);
+    card.style.setProperty('--rotate-y', `${rotateY}deg`);
+  }
 
+  resetTilt(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    card.style.setProperty('--rotate-x', `0deg`);
+    card.style.setProperty('--rotate-y', `0deg`);
+  }
 }
